659c174efb40d28c91718c1449e34a223db6c112
Daraul Harris <thurst306@gmail.com>
2015-06-11 18:04:33 +0000

    Draggable markers!
    > Now if only I could get something to happen when I finish dragging the marker...
    
    > Also added the markers ID

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,6 +14,7 @@ class PlacesController < ApplicationController
       marker.lat place.latitude
       marker.lng place.longitude
       marker.infowindow place.description 
+      marker.json({:id => "place_#{place.id}_marker" })
     end
   end
 
