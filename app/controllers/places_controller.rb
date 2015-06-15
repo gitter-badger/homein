84ffbde1cb8b6230864968d5467c247de7bf12d8
Daraul Harris <thurst306@gmail.com>
2015-06-15 17:38:58 -0400

    Small tweaks with the buttons

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -29,6 +29,8 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new(place_params)
 
+    @place.user = current_user 
+
     respond_to do |format|
       if @place.save
         format.html { redirect_to @place, notice: 'Place was successfully created.' }
