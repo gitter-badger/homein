6ba35ee4fa7d5e7eca2c72f0fc08214b6da23698
Daraul Harris <thurst306@gmail.com>
2015-06-15 16:45:19 -0400

    Basic user authentication added Just need to add authorization now

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
   before_action :set_place, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :create, :new]
 
   # GET /places
   # GET /places.json
