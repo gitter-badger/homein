166b23647b6c810016b3bac296458d965d11dde6
Daraul Harris <thurst306@gmail.com>
2015-07-19 20:00:28 +0000

    Got turbolinks and jquery to play nice

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require jquery-ui
-//= require turbolinks
 //= require algolia/v3/algoliasearch.min
+//= require turbolinks
 //= require_tree .
