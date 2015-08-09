f01fb0005ecde76dd5d92227f8a50417d958de39
Daraul Harris <thurst306@gmail.com>
2015-08-09 19:08:24 +0000

    Destroying a place now destroys its dependents The same should hold true for destroying pictures

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	has_many :pictures 
+	has_many :pictures, :dependent => :destroy 
 	
 	accepts_nested_attributes_for :pictures, :allow_destroy => true
 	
