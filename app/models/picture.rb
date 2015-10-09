50f9143b49f3e959f418b8dcdf2be72edd33f22d
Daraul <thurst306@gmail.com>
2015-10-09 23:59:25 +0000

    I had some typos in the file type validation

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,5 +5,5 @@ class Picture < ActiveRecord::Base
 	
 	validates_with AttachmentPresenceValidator, attributes: :image 
 	validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes, :message => "Images should be smaller than 1MB"
-	validates_with AttachmentContentTypeValidator, attributes: :image, content_type: { content_type: [ "image/jpeg", "image/png" ] }, :message => "JPEG or PNG files only, please!"
+	validates_with AttachmentContentTypeValidator, attributes: :image, :content_type => [ "image/jpeg", "image/png" ], :message => "JPEG or PNG files only, please!"
 end
