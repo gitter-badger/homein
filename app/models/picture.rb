3e6d1cc709d163523ed3ae245db4b4ae962b5b8a
Daraul <thurst306@gmail.com>
2015-10-09 23:24:44 +0000

    Added some validations for images

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,5 +2,8 @@ class Picture < ActiveRecord::Base
     belongs_to :place 
     
     has_attached_file :image, :default_url => ":rails_root/public/system/:class/:attachment/missing.png"
-	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	
+	validates_with AttachmentPresenceValidator, attributes: :image 
+	validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes, :message => "Images should be smaller than 1MB"
+	validates_with AttachmentContentTypeValidator, attributes: :image, content_type: { content_type: [ "image/jpeg", "image/png" ] }, :message => "JPEG or PNG files only, please!"
 end
