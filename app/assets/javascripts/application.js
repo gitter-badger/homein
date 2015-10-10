233beddb7b44ddc396bc212f754ec5919e93f0f4
Daraul <thurst306@gmail.com>
2015-10-10 00:28:46 +0000

    Added rstacruz's nprogress progress bars There's another rubygems option but that didn't work for some reason It also seemed like too much being that that's another gem for the app to depend on

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,7 @@
 //= require algolia/v3/algoliasearch.min
 //= require turbolinks
 //= require_tree .
+
+$(document).on('page:fetch',   function() { NProgress.start(); });
+$(document).on('page:change',  function() { NProgress.done(); });
+$(document).on('page:restore', function() { NProgress.remove(); });
