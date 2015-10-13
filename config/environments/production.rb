82872ab70034fc505dea2050337e205c47c64452
Daraul <thurst306@gmail.com>
2015-10-13 22:28:06 +0000

    Production settings work Just added a small change to abstract the production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
     # Disable delivery errors, bad email addresses will be ignored
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.default_url_options = { :host => 'floating-headland-8577.heroku.com' }
+    config.action_mailer.default_url_options = { :host => ENV['production_host'] }
     ActionMailer::Base.smtp_settings = {
       :address    => "smtp.sendgrid.net",
       :port       => 25,
